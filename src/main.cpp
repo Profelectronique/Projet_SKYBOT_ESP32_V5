bb3b328fc9c0269df1580b3f6f4dfa20d023e65a
Prof <Profelectronique@gmail.com>
2024-09-19 17:36:35 +0200

    Fréquence clignotement 1Hz

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,7 @@ void setup()
 void loop()
  {
  digitalWrite(led, HIGH);// Ecrit la valeur HIGH (=1) sur la broche GPIO16
- delay(1000); // Attendre 1000ms
+ delay(500); // Attendre 1000ms
  digitalWrite(led, LOW); // Ecrit la valeur LOW (=0) sur la broche GPIO16
- delay(1000);
+ delay(500);
 }
