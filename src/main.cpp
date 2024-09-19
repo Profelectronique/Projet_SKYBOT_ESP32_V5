cb3e332185cb3d2d2797b082b3243b88f3f6be90
Prof <Profelectronique@gmail.com>
2024-09-19 17:28:37 +0200

    Led clignotante

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,15 @@
+/////////// LED Clignotante //////////////
 #include <Arduino.h>
+int led = 16;//Déclaration de la variable led, comme un entier (integer)
+void setup()
+ {
+ pinMode(led, OUTPUT); // Configure la broche GPIO16 en sortie
+ }
 
-// put function declarations here:
-int myFunction(int, int);
-
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void loop()
+ {
+ digitalWrite(led, HIGH);// Ecrit la valeur HIGH (=1) sur la broche GPIO16
+ delay(1000); // Attendre 1000ms
+ digitalWrite(led, LOW); // Ecrit la valeur LOW (=0) sur la broche GPIO16
+ delay(1000);
 }
